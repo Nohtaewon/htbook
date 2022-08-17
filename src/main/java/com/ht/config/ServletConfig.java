be6c0f1d0b5c7ce376ca4b60541499a6e4e28f57
kimhyukgyu <hyukgyu3818@naver.com>
2022-08-17 23:06:43 +0900

    Project환경구축,테스트완료

---
+++ src/main/java/com/ht/config/ServletConfig.java
@@ -0,0 +1,47 @@
+package com.ht.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc //설정이 완료된 여러 스프링 빈을 추가해준다.
+@ComponentScan(basePackages = {"a.b.controller"})
+@ComponentScan(basePackages = {"a.b.exception"} )
+public class ServletConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+	
+/*
+ * 내용의 의미는 /resources/** 말대로 /resources/js, 
+ * /resources/css 와 같은 경로로 명시하는 경우에 
+* /resources/ 경로로 해석하겠다 라는 의미로 보시면 됩니다. 
+ 
+ */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		
+	}
+	
+	@Bean
+	public MultipartResolver multipartResolver() {
+		StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+		return resolver;
+	}
+
+}
---
+++ src/main/java/com/ht/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.ht.config;
+
+
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+	
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		// url 경로가 없으면 예외 처리
+		registration.setInitParameter("throwExceptionIfNoHandlerFound","true");
+		
+		// 파일 업로드를 위한 MultipartConfig
+		MultipartConfigElement muConfigElement = new MultipartConfigElement("C:\\storage\\temp",20971520, 41943040, 20971520);
+		registration.setMultipartConfig(muConfigElement);
+	}
+	
+	// 웹에서 등록할 때 한글 utf-8 허용 필터
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		//return super.getServletFilters();
+		return new Filter[] {characterEncodingFilter};
+	}
+
+	
+}
