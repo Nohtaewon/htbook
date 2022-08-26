3bba43502f13f17745da270135eeef61c53ffe74
kimhyukgyu <hyukgyu3818@naver.com>
2022-08-26 16:34:31 +0900

    관리자,회원 인터셉터적용 구현

--- src/main/java/com/ht/config/WebConfig.java
+++ src/main/java/com/ht/config/WebConfig.java
@@ -45,6 +45,8 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 		//return super.getServletFilters();
 		return new Filter[] {characterEncodingFilter};
 	}
+	
+	
 
 	
 }
