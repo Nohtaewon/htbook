03b86834fd422f3abdf02100d3d2478f929e215b
Nohtaewon <gkgk713@naver.com>
2022-09-12 01:52:01 +0900

    관리자계정 경로추가, 관리자 메인 페이지 수정

--- src/main/java/com/ht/config/ServletConfig.java
+++ src/main/java/com/ht/config/ServletConfig.java
@@ -54,6 +54,7 @@ public class ServletConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/member/login");
         registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/admin/**");
+        registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/book/**");
         registry.addInterceptor(new CartInterceptor()).addPathPatterns("/cart/**").excludePathPatterns("/cart/add");
     }
 
